00e0d3e2665060eb09e8d754eb38d5eddb60887a
Andre Bauer <76748713+bauer-code@users.noreply.github.com>
2022-10-06 18:45:42 +0000

    branch testando

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 print("Hello World 2")
+
+print("Hello World 2")
+
+a = 1
+b = 2
+c = 2
+d = 4
+
+
+def f(a,b, v = 3, z = 4):
+  global d
+  c = a + b + d + v + z
+  return c
+  
+
+
+print(f(2, 3, z = 2))
+
+print(f"a={a} b={b}")
 #comment
